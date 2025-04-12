994affc6e1edf514a6d7b0c00c436e5d16a2b8e3
liuguowei <1085550655@qq.com>
2025-04-12 15:20:07 +0800

    feat: 处理响应数据

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { bulidURL } from './helpers/url'
-import { AxiosRequestConfig } from './types'
+import { AxiosPromise, AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import { transformRequest } from './helpers/data'
 import { processHeaders } from './helpers/headers'
 
-function axios(config: AxiosRequestConfig): void {
+function axios(config: AxiosRequestConfig): AxiosPromise  {
   processConfig(config)
-  xhr(config)
+  return xhr(config)
 }
 
 function processConfig (config: AxiosRequestConfig): void {
