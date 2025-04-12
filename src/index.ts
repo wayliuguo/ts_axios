1e21bfd020291d80296a4d3185a56f68e5959033
liuguowei <1085550655@qq.com>
2025-04-12 15:04:06 +0800

    feat: 处理请求url/body/header 数据

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
+import { bulidURL } from './helpers/url'
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 
 function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
   xhr(config)
+}
+
+function processConfig (config: AxiosRequestConfig): void {
+  config.url = transformUrl(config)
+  config.headers = transformHeaders(config)
+  config.data = transformRequest(config.data)
+}
+
+function transformUrl (config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return bulidURL(url, params)
+}
+
+function transformHeaders (config: AxiosRequestConfig) {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 
 export default axios
