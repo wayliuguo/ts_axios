aa766d426091884be8b289b6d03d041840e689a0
liuguowei <1085550655@qq.com>
2025-04-26 12:15:35 +0800

    feat: 静态方法扩展

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -6,7 +6,7 @@ import {
   Interceptors,
   PromiseChain
 } from '../types'
-import dispatchRequest from './dispatchRequest'
+import dispatchRequest, { transformURL } from './dispatchRequest'
 import InterceptorManager from './InterceptorManager'
 import mergeConfig from './mergeConfig'
 
@@ -86,6 +86,11 @@ export default class Axios {
 
   patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
     return this._requestMethodWithData('patch', url, data, config)
+  }
+
+  getUri(config?: AxiosRequestConfig): string {
+    config = mergeConfig(this.defaults, config)
+    return transformURL(config)
   }
 
   _requestMethodWithoutData(method: Method, url: string, config?: AxiosRequestConfig) {
